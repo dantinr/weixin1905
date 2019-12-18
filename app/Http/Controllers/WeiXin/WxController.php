28906ba4174f2fedaaf810ad69b618f408ec067f
dante <dantinr@gmail.com>
2019-12-18 10:46:48 +0800

    &

--- app/Http/Controllers/WeiXin/WxController.php
+++ app/Http/Controllers/WeiXin/WxController.php
@@ -329,7 +329,7 @@ class WxController extends Controller
                 [
                     'type'  => 'view',
                     'name'  => '投票',
-                    'url'   => 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx86737d80f8dac5c9&redirect_uri='.$redirect_uri.'&response_type=code&scope=snsapi_userinfostate=ABCD1905#wechat_redirect'
+                    'url'   => 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx86737d80f8dac5c9&redirect_uri='.$redirect_uri.'&response_type=code&scope=snsapi_userinfo&state=ABCD1905#wechat_redirect'
                 ],
             ]
         ];
