cf939aaba00daf9d475ac1eb3444872eb6b25dd6
dante <dantinr@gmail.com>
2019-12-23 15:48:40 +0800

    share

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -633,7 +633,7 @@
         wx.updateAppMessageShareData({
             title: '分享测试', // 分享标题
             desc: '描述', // 分享描述
-            link: 'http://wx1905.comcto.com', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
+            link: 'http://wx1905.comcto.com/img/2ha.jpg', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
             imgUrl: 'http://wx1905.comcto.com/img/2ha.jpg', // 分享图标
             success: function () {
                 // 设置成功
